b931f7462a11056fcf8c594cd6c6b818b21e3848
regenalgrant <regenal@mac.com>
2016-11-08 15:58:35 -0800

    finish mine texting now

--- starter-code/scripts/models/repo.js
+++ starter-code/scripts/models/repo.js
@@ -2,11 +2,19 @@
   var repos = {};
 
   repos.allRepos = [];
-// TODO: create a githubToken.js file that we can use to generate our headers
+// DONE: create a githubToken.js file that we can use to generate our headers
          // properly.
   repos.requestRepos = function(callback) {
     /* TODO: How would you like to fetch your repos? Someone say AJAX?!
       Do not forget to call the callback! */
+    $.ajax({
+      url: 'https://api.github.com',
+      type:'GET',
+      headers: {'Authorization': 'token ' + githubToken},
+      success: function(data, message,xhr){
+        console.log(data);
+      }
+    });
   };
 
   repos.withTheAttribute = function(myAttr) {
