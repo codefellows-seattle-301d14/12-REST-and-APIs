a4d921ea4e6377ec327d12371159aae27072d4d9
kcirekcom <erick.f.mock@gmail.com>
2016-11-08 16:10:37 -0800

    set data equal to repos.allRepos and invoked the callback

--- starter-code/scripts/models/repo.js
+++ starter-code/scripts/models/repo.js
@@ -8,11 +8,13 @@
     /* TODO: How would you like to fetch your repos? Someone say AJAX?!
       Do not forget to call the callback! */
     $.ajax({
-      url: 'https://api.github.com',
+      url: 'https://api.github.com/users/codefellows-seattle-301d14/repos',
       type:'GET',
       headers: {'Authorization': 'token ' + githubToken},
-      success: function(data, message,xhr){
+      success: function(data, message, xhr){
         console.log(data);
+        repos.allRepos = data;
+        callback();
       }
     });
   };
